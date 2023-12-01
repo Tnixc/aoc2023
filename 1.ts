e2d075fd4e05af03ad4c1e90483c3a555375dfe9
Tnixc <tnixxc@gmail.com>
2023-12-01 01:22:09 -0500

    this should 100% work but it doesn't

--- 1.ts
+++ 1.ts
@@ -1010,11 +1010,10 @@ const x = [
   "7pqrstsixteen",
 ];
 const x2 = [
-"1abc2",
-"pqr3stu8vwx",
-"a1b2c3d4e5f",
-"treb7uchet"
-
+  "1abc2",
+  "pqr3stu8vwx",
+  "a1b2c3d4e5f",
+  "treb7uchet"
 ]
 
 var total = 0;
